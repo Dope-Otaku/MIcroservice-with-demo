773968ff9959a91d684627ce85abe0ae053bb441
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-28 12:31:37 +0530

    created some routes to test the application

--- src/app.py
+++ src/app.py
@@ -1,1 +1,16 @@
-print("hello world!")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def heelo_world():
+    return "<p>Hey gusy wassup!</p>"
+
+@app.route("/help")
+def help():
+    return jsonify(
+        message = "hello"
+    )
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
